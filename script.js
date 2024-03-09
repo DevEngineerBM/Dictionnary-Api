fa2b4a193b67f66eac7c7dc0860fa787679d119b
belkhalil <belkhalil1mohamed@gmail.com>
2024-03-09 13:50:28 +0100

    WIP: ADD PLAY SOUND FUNCTIONNALITY

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const url = 'https://api.dictionaryapi.dev/api/v2/entries/en/';
 const input = document.getElementById('input');
 const btnSearch = document.getElementById('btn-search');
 const result = document.getElementById('under-box');
-
+const sound = document.getElementById('sound');
 // DEFINE THE SEARCH FUNCTION 
 function search() {
   
@@ -22,7 +22,7 @@ function search() {
             return response.json();
         })
         .then((data) => {
-            // If the data has the expected structure...
+            // IF DATA  HAS THE EXPECTED STRUCTURE
             if (data && data[0] && data[0].meanings && data[0].meanings[0]) {
               
                 console.log(data);
@@ -30,7 +30,7 @@ function search() {
                 result.innerHTML = 
                 `<div class="result">
                     <h3>${word}</h3>
-                    <button>
+                    <button id = 'speaker' >
                         <i class="fa-solid fa-volume-high"></i>
                     </button>
                 </div>
@@ -44,15 +44,37 @@ function search() {
                 <p class="word-example">
                     ${data[0].meanings[0].definitions[0].example || ' example not found in dictionary'}
                 </p>`;
+
+            // DEFINE  THE AUDIO URL
+      let audioUrl = data[0].phonetics[0].audio || data[0].phonetics[1].audio;
+
+        // SET THE AUDIO SOURCE
+        sound.setAttribute('src', audioUrl);
+
+    
+   setTimeout(function() {
+
+      let playBtn = document.getElementById('speaker')
+      function playSound(){
+      sound.play()
+    }
+
+playBtn.addEventListener('click', playSound )
+
+},0)
+
             } else {
-                // IF THE DATA NOT HAVE THE EXPECTED STRUCTURE, LOG AN ERROR
-                console.error('Unexpected API response', data);
+                // IF THE DATA NOT HAVE THE EXPECTED STRUCTURE, ALERT/LOG AN ERROR
+                alert('Unexpected API response', data);
+                console.error(object);('Unexpected API response', data)
             }
         })
         .catch((error) => {
-            // IF THERE WAS  AN ERROR WITH THE FETCH OPERATION, LOG IT
+            // IF THERE WAS  AN ERROR WITH THE FETCH OPERATION, ALERT/LOG IT
+            alert('There has been a problem with your fetch operation:', error);
             console.error('There has been a problem with your fetch operation:', error);
         });
-}
+    }
 
 btnSearch.addEventListener('click', search);
+
